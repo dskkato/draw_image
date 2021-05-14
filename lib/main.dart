f866cdfd740043bbc9f7d4dc8574ee5ea1992a6d
dskkato <kato.daisuke429@gmail.com>
2021-05-15 01:27:10 +0900

    マスクを描画できるところまで実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,173 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'package:quiver/iterables.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String? title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class DrawingArea {
+  List<Offset> points = [];
+  Paint paint;
+  bool isClose = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  get length => points.length;
+
+  DrawingArea(this.paint);
+}
+
+class _HomeState extends State<Home> {
+  List<DrawingArea> areas = [];
+  final _minDistance = 5.0;
+  bool _isDrawing = false;
+  static List<Color> _colors = [
+    Color.fromARGB(128, 255, 0, 0),
+    Color.fromARGB(128, 0, 255, 0),
+    Color.fromARGB(128, 0, 0, 255),
+  ];
+  Iterator<Paint> _closePaints = cycle(List<Paint>.generate(
+      _colors.length,
+      (index) => Paint()
+        ..color = _colors[index]
+        ..style = PaintingStyle.fill
+        ..strokeWidth = 0.0)).iterator
+    ..moveNext();
+  Iterator<Paint> _openPaints = cycle(List<Paint>.generate(
+      _colors.length,
+      (index) => Paint()
+        ..color = _colors[index]
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 3.0)).iterator
+    ..moveNext();
+
+  final Image image = Image.asset(
+    'assets/fruit.jpg',
+  );
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title!),
+        title: Text('MyDrawer'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          child: GestureDetector(
+            onPanUpdate: (details) {
+              var point = details.localPosition;
+              if (_isDrawing) {
+                final points = areas.last.points;
+                final startPoint = points.first;
+                if (points.length > 10 &&
+                    (point - startPoint).distance < _minDistance) {
+                  // close the path
+                  setState(() {
+                    areas.last.points.add(point);
+                    areas.last.isClose = true;
+                    areas.last.paint = _closePaints.current;
+                    _closePaints.moveNext();
+                    _isDrawing = false;
+                  });
+                } else {
+                  // update the path
+                  setState(() {
+                    areas.last.points.add(point);
+                  });
+                }
+              }
+            },
+            onPanStart: (details) {
+              var point = details.localPosition;
+              if (_isDrawing) {
+                final points = areas.last.points;
+                final startPoint = points.first;
+                if ((point - startPoint).distance < _minDistance) {
+                  // close the path
+                  setState(() {
+                    areas.last.points.add(point);
+                    areas.last.isClose = true;
+                    areas.last.paint = _closePaints.current;
+                    _closePaints.moveNext();
+                    _isDrawing = false;
+                  });
+                } else {
+                  // update the path
+                  setState(() {
+                    areas.last.points.add(point);
+                  });
+                }
+              } else {
+                // start a path
+                final area = DrawingArea(_openPaints.current);
+                area.points.add(point);
+
+                setState(() {
+                  areas.add(area);
+                  _openPaints.moveNext();
+                  _isDrawing = true;
+                });
+              }
+            },
+            child: ClipRect(
+              child: Stack(
+                children: [
+                  image,
+                  CustomPaint(
+                    painter: DrawingPainter(areas),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DrawingPainter extends CustomPainter {
+  final List<DrawingArea> areas;
+  DrawingPainter(this.areas);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (var area in areas) {
+      final points = area.points;
+      final paint = area.paint;
+      if (area.isClose && points.length > 0) {
+        var path = Path();
+
+        var it = points.iterator..moveNext();
+        path.moveTo(it.current.dx, it.current.dy);
+        while (it.moveNext()) {
+          path.lineTo(it.current.dx, it.current.dy);
+        }
+        canvas.drawPath(path, paint);
+      } else {
+        canvas.drawPoints(ui.PointMode.polygon, points, paint);
+        canvas.drawCircle(points.first, 5, Paint()..color = Colors.black);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DrawingPainter old) {
+    return old.areas == areas;
   }
 }
